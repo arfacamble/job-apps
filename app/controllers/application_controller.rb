516a5b94baf59237ca7e0761f7b23cd0501ebfd9
Arthur Campbell <arfa.camble@gmail.com>
2021-08-31 19:30:49 +0100

    api response headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  after_filter :cors
 
-  def cors
-    response.headers['Access-Control-Allow-Origin'] = '*'
-  end
 end
